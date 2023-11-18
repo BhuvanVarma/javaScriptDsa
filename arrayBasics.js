c5d9a74578c993379c689f1039dbdeb32b858ea9
Bhuvan Varma Konduru <bhuvan.varma123@gmail.com>
2023-11-18 19:55:24 +0530

    added first file

---
+++ arrayBasics.js
@@ -0,0 +1,13 @@
+// Array decalaration
+
+let person = {
+    name : "bhuvan",
+    age : 22,
+};
+let arr = ["apple", "banana", person]
+
+
+
+console.log(arr[2].name)
+
+// it can store any data type
